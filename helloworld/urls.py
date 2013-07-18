0e5ec04f57a9ea619273a087c5731d461f38e69b
Kwan Lee <kwan@media.mit.edu>
2013-07-18 16:13:24 +0900

    added local bootstrap fixed #1

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -8,7 +8,9 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'polls.views.home', name='home'),
     # url(r'^helloworld/', include('helloworld.foo.urls'))
-    url(r'^polls/','polls.views.polls', name = 'polls'),
+    url(r'^polls/$', 'polls.views.polls', name = 'polls_list'),
+    url(r'^polls/process/', 'polls.views.polls_process', name = 'polls_process'),
+    url(r'^polls/radio/', 'polls.views.polls_radio', name = 'polls_radio'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,10 @@
 # Create your views here.
 from django.shortcuts import render
-from polls.models import Poll
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.urlresolvers import reverse
+
+from polls.models import Poll, Choice
 
 def home(request):
     data = {}
@@ -11,3 +15,43 @@ def polls(request):
     data = {}
     data['polls'] = Poll.objects.all()
     return render(request, 'polls.html', data)
+
+def polls_process(request):
+    data = {}
+
+    print "Hello"
+    if request.method == "POST":
+
+        choice_id = request.POST["choice"]
+
+        print "Choice ID submitted %d" % int(choice_id)
+        choice_obj = Choice.objects.get(id=choice_id)
+        choice_obj.votes += 1
+        choice_obj.save()
+    else:
+        print "Request is not POST"
+
+    return HttpResponseRedirect(reverse("polls_list"))
+
+@csrf_exempt
+def polls_radio(request):
+    import json
+
+    data = {}
+
+    if request.method == "POST":
+        radio_id = request.POST["radio_id"]
+        print "We received radio ID: %s" % radio_id
+
+        choice_id = int(radio_id[1:])
+        choice_obj = Choice.objects.get(id=choice_id)
+        choice_obj.votes += 1
+        choice_obj.save()
+
+        data["radio_id"] = "v%d" % choice_id
+        data["votes"] = choice_obj.votes
+
+    else:
+        print "Invalid radio button request"
+
+    return HttpResponse(json.dumps(data), mimetype="application/json")
